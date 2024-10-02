54d8f1295588d1f0cbd0bff689608837d8cdf9a5
Nicole0150 <nicole.williamson@barlcays.com>
2024-10-02 12:41:15 +0100

    first manager

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,5 +1,18 @@
-class Manager {
+const manager = require("./Employee");
 
+class Manager extends Employee{
+    #employeesManaged
+    constructor(department){
+        this.department = department;
+        this.#employeesManaged =[];
+    }
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
+    addEmployeeManaged(employee) {
+        this.#employeesManaged.push(employee);
+        return this.#employeesManaged;
+    }
 
 
     
